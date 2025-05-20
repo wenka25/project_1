5eecf35295cc0a72aa23a093144f3b132e8512bd
wenka25 <wenkasalinding04@gmail.com>
2025-05-20 09:51:38 +0800

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,55 @@
+<?php
+include 'koneksi.php';
+
+$query = "SELECT * FROM pendaftaran";
+$result = mysqli_query($conn, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Data Pendaftar</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="tampil.css">
+</head>
+
+<body>
+<div class="container mt-5">
+  <h1 class="mb-4">Daftar Pemain Futsal</h1>
+  <table class="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Nama Lengkap</th>
+        <th>Tempat Lahir</th>
+        <th>Tanggal Lahir</th>
+        <th>Alamat</th>
+        <th>No HP</th>
+        <th>Email</th>
+        <th>Usia</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php
+      $no = 1;
+      while($data = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <td>{$no}</td>
+                <td>{$data['nama_lengkap']}</td>
+                <td>{$data['tempat_lahir']}</td>
+                <td>{$data['tanggal_lahir']}</td>
+                <td>{$data['alamat_lengkap']}</td>
+                <td>{$data['no_hp']}</td>
+                <td>{$data['email']}</td>
+                <td>{$data['usia']}</td>
+              </tr>";
+        $no++;
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
