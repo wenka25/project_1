ec3743bb387caacb3a13e6d6b5ce46b8f91746ec
wenka25 <wenkasalinding04@gmail.com>
2025-05-26 12:12:21 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "wenka123";
-$database = "db_futsal";
+<?php //Menandakan awal dari kode PHP
+
+//Konfigurasi Database
+$servername = "localhost"; //Variabel untuk menyimpan lokasi server database
+$username = "root"; //Nama pengguna untuk mengakses database
+$password = "wenka123"; //Kata sandi untuk autentikasi user database
+$database = "db_futsal"; //Nama database yang akan digunakan
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database); //untuk menghubungkan ke MySQL
+
+?>
